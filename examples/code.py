841786ccf82318352c7b6473114035bc1b1c9a23
SunKyu Lee <leesunkeuy@naver.com>
2015-02-23 02:35:49 +0900

    change some number

--- examples/code.py
+++ examples/code.py
@@ -2,7 +2,7 @@ import subprocess, os
 import sys
 import time
 f = open("table.txt",'r')
-cycle =10
+cycle =20
 while 1: 
   line = f.readline()
   if not line:
@@ -12,7 +12,7 @@ while 1:
     continue
   linetable = line.split('/')
   url = "coap://[%s]/sensors?addr=%s,type=%s" %(linetable[1], linetable[2], linetable[3])
-  arg = ["-m", "get" ,"-B","3", url, linetable[0]]
+  arg = ["-m", "get" ,"-B","1", url, linetable[0]]
   arg.append(url)
   pathname = os.path.dirname(sys.argv[0])
   path = os.path.abspath(pathname)
