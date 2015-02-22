eb42b1a05b22fa4a04849705536e32a578f9e3b5
SunKyu Lee <leesunkeuy@naver.com>
2015-02-23 00:42:46 +0900

    fix manythings

--- examples/code.py
+++ examples/code.py
@@ -1,11 +1,15 @@
 import subprocess, os
-
+import sys
 PIPE = subprocess.PIPE
 arg = "-m get -o result.txt coap://[aaa::0212:7400:15d5:a731]/sensors?addr=8003,type=all"
-p = subprocess.Popen(["/Users/leesunkyu/Dropbox/Soucecode/2015-1/HUFS_libcoap_gateway/examples/coap-client", arg ], stdin=PIPE, stdout=PIPE)
+pathname = os.path.dirname(sys.argv[0])
+path = os.path.abspath(pathname)
+path = path + "/coap-client"
+print path
+p = subprocess.Popen([path, arg ])
 
-p.stdin.write("10")
-p.stdin.flush()
-print p.stdout.read() #Deadlock
+#p.stdin.write("10")
+#p.stdin.flush()
+#print p.stdout.read() #Deadlock
 print "End of Execution"
 os.system("PAUSE")
