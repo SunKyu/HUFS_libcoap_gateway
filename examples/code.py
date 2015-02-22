2e3e146279b79cce4d17d5846cd1ce8a94baf7d6
nasang <sktkddusrj@naver.com>
2015-02-22 16:40:27 +0000

    simple while loop

--- examples/code.py
+++ examples/code.py
@@ -1,12 +1,15 @@
 import subprocess, os
 import sys
 
-arg = ["-m", "get" ,"-o", "result.txt","-B","3","coap://[aaa::0212:7400:15d5:a731]/sensors?addr=8003,type=all"]
-pathname = os.path.dirname(sys.argv[0])
-path = os.path.abspath(pathname)
-path = path + "/coap-client"
-print path
+i = 3
+while i: 
+    arg = ["-m", "get" ,"-o", "result.txt","-B","3","coap://[aaa::0212:7400:15d5:a731]/sensors?addr=8003,type=all"]
+    pathname = os.path.dirname(sys.argv[0])
+    path = os.path.abspath(pathname)
+    path = path + "/coap-client"
+    print path
 
-p = subprocess.Popen([path]+ arg)
-p.wait()
-print "End of Execution"
+    p = subprocess.Popen([path]+ arg)
+    p.wait()
+    print "End of Execution"
+    i = i-1
