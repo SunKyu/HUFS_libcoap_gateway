59b214889228cc8911c3e6ea740a886aea0b3f66
nasang <sktkddusrj@naver.com>
2015-02-22 16:36:46 +0000

    add limit time arg

--- examples/code.py
+++ examples/code.py
@@ -1,7 +1,7 @@
 import subprocess, os
 import sys
 
-arg = ["-m", "get" ,"-o", "result.txt","coap://[aaa::0212:7400:15d5:a731]/sensors?addr=8003,type=all"]
+arg = ["-m", "get" ,"-o", "result.txt","-B","3","coap://[aaa::0212:7400:15d5:a731]/sensors?addr=8003,type=all"]
 pathname = os.path.dirname(sys.argv[0])
 path = os.path.abspath(pathname)
 path = path + "/coap-client"
