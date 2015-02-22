e17f33d1f889c2175f576bef6a83c9197ce9a462
SunKyu Lee <leesunkeuy@naver.com>
2015-02-23 01:49:33 +0900

    file read

--- examples/code.py
+++ examples/code.py
@@ -2,14 +2,24 @@ import subprocess, os
 import sys
 
 i = 3
+f = open("table.txt",'r')
+cycle =10
 while i: 
-    arg = ["-m", "get" ,"-o", "result.txt","-B","3","coap://[aaa::0212:7400:15d5:a731]/sensors?addr=8003,type=all"]
-    pathname = os.path.dirname(sys.argv[0])
-    path = os.path.abspath(pathname)
-    path = path + "/coap-client"
-    print path
+  line = f.readline()
+  if not line:
+    f.seek(0)
+    print "cycle time is %d" %cycle
+    sleep(cycle)
+    continue
+  linetable = line.split('/')
+  url = "coap://[%s]/sensors?addr=%s,type=%s" %(linetable[1], linetable[3], linetable[3]
+  arg = ["-m", "get" ,"-o", "result.txt","-B","3", url]
+  pathname = os.path.dirname(sys.argv[0])
+  path = os.path.abspath(pathname)
+  path = path + "/coap-client"
+  print path
 
-    p = subprocess.Popen([path]+ arg)
-    p.wait()
-    print "End of Execution"
-    i = i-1
+  p = subprocess.Popen([path]+ arg)
+  p.wait()
+  print "End of Execution"
+  i = i-1
