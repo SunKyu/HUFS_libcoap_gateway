75f27114a4ea8b547615927d9ebee3ee291dc513
SunKyu Lee <leesunkeuy@naver.com>
2015-02-23 00:54:19 +0900

    fix

--- examples/code.py
+++ examples/code.py
@@ -1,15 +1,12 @@
 import subprocess, os
 import sys
-PIPE = subprocess.PIPE
+
 arg = "-m get -o result.txt coap://[aaa::0212:7400:15d5:a731]/sensors?addr=8003,type=all"
 pathname = os.path.dirname(sys.argv[0])
 path = os.path.abspath(pathname)
 path = path + "/coap-client"
 print path
+
 p = subprocess.Popen([path, arg ])
 
-#p.stdin.write("10")
-#p.stdin.flush()
-#print p.stdout.read() #Deadlock
 print "End of Execution"
-os.system("PAUSE")
